3fdd93c9ddc56506737bc3ec6512895809a89b9a
fabiansi <fabiansi@kth.se>
2022-10-05 13:50:56 +0200

    [ADD] testscript

---
+++ fabi_scripts/run_sph_tests.sh
@@ -0,0 +1,49 @@
+
+# python /fabi_project/p3_tract_ttl/TrackToLearn/runners/test.py \
+#    /fabi_project/data/prelim_results/000_0deg \
+#    try_to_get_learning \
+#    wm-mask \
+#    /fabi_project/data/scratch/rot_test/0_0deg/fibercup2.hdf5 \
+#    fibercup2 \
+#    /fabi_project/data/scratch/rot_test/0_0deg/wm.nii.gz \
+#    /fabi_project/data/fibercup_ttl/scoring_data \
+#    /fabi_project/data/prelim_results/00/ \
+#    /fabi_project/data/prelim_results/00/hyperparameters.json \
+#    --remove_invalid_streamlines
+# 
+# python /fabi_project/p3_tract_ttl/TrackToLearn/runners/test.py \
+#    /fabi_project/data/prelim_results/001_90deg \
+#    try_to_get_learning \
+#    wm-mask \
+#    /fabi_project/data/scratch/rot_test/1_90deg/fibercup2.hdf5 \
+#    fibercup2 \
+#    /fabi_project/data/scratch/rot_test/1_90deg/wm-90deg.nii.gz \
+#    /fabi_project/data/fibercup_ttl/scoring_data \
+#    /fabi_project/data/prelim_results/00/ \
+#    /fabi_project/data/prelim_results/00/hyperparameters.json \
+#    --remove_invalid_streamlines
+# 
+ python /fabi_project/p3_tract_ttl/TrackToLearn/runners/test.py \
+    /fabi_project/data/prelim_results/002_180deg \
+    try_to_get_learning \
+    wm-mask \
+    /fabi_project/data/scratch/rot_test/2_180deg/fibercup2.hdf5 \
+    fibercup2 \
+    /fabi_project/data/scratch/rot_test/2_180deg/wm-180deg.nii.gz \
+    /fabi_project/data/fibercup_ttl/scoring_data \
+    /fabi_project/data/prelim_results/00/ \
+    /fabi_project/data/prelim_results/00/hyperparameters.json \
+    --remove_invalid_streamlines
+# 
+# python /fabi_project/p3_tract_ttl/TrackToLearn/runners/test.py \
+#    /fabi_project/data/prelim_results/003_270deg \
+#    try_to_get_learning \
+#    wm-mask \
+#    /fabi_project/data/scratch/rot_test/3_270deg/fibercup2.hdf5 \
+#    fibercup2 \
+#    /fabi_project/data/scratch/rot_test/3_270deg/wm-270deg.nii.gz \
+#    /fabi_project/data/fibercup_ttl/scoring_data \
+#    /fabi_project/data/prelim_results/00/ \
+#    /fabi_project/data/prelim_results/00/hyperparameters.json \
+#    --remove_invalid_streamlines
+
